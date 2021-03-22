2813328f78220ebe5b756d247e8d3919b563cadd
ShreyasAmbre <shreyasambre@teradoengineering.com>
2021-03-22 17:12:49 +0530

    first test commit

---
+++ src/app/home/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {MatExpansionPanel} from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
+})
+export class Tab2Page implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
